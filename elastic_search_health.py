457f31f6052eb222a5108502ade9a2775b1ba0a8
Egidijus <egidijus@anotherlan.com>
2019-02-28 18:05:38 +0000

    i think i broke it , but the idea is to loop over the health list, and if any are > 0, then we carry on the loop

--- elastic_search_health.py
+++ elastic_search_health.py
@@ -1,4 +1,4 @@
-#!/bin/env python 
+#!/bin/env python
 
 import requests
 import json
@@ -11,6 +11,8 @@ from datetime import datetime
 API_URL = "http://elk-elasticsearch.service.prod.consul:9200"
 HEALTH = "/_cluster/health"
 SHARDS = "/_cat/shards?format=json"
+STATS = "/_cluster/stats?human&pretty"
+
 #http://elk-elasticsearch.service.prod.consul:9200/_cat/shards?format=json
 
 
@@ -26,6 +28,7 @@ def client(API_URL, PATH):
             verify=False,
         )
         state = get_state.json()
+        # print(state)
         return state
     except Exception as e:
         print(
@@ -34,44 +37,18 @@ def client(API_URL, PATH):
             e,
         )
 
-def the_average(lst): 
-    return sum(lst) / len(lst)
 
-def health_average(start_time, health_json):
-    try:
-        relocating = []
-        initializing = []
-        unassigned = []
-        loop_start = time.time()
-        count = 10
-        """
-        relocating_shards: 0,
-        initializing_shards: 32,
-        unassigned_shards: 233,
-        """
-        while count != 0:
-            time.sleep(2.0 - ((time.time() - loop_start) % 2.0))
-            count -= 1
-            print("STARTED::", start_time, "NOW::", datetime.now(), count)
-            print(health_json['relocating_shards'])
-            print(health_json['initializing_shards'])
-            print(health_json['unassigned_shards'])
-            relocating.append(health_json['relocating_shards'])
-            initializing.append(health_json['initializing_shards'])
-            unassigned.append(health_json['unassigned_shards'])
-        print("relocating AVG", round(the_average(relocating), 2))
-        print("initializing AVG", round(the_average(initializing), 2))
-        print("unassigned AVG", round(the_average(unassigned), 2))
-        # print("number of samples", len(health))
-        return round(the_average(health), 2)
-    except Exception as e:
-        print(
-            "Error on line {}".format(sys.exc_info()[-1].tb_lineno),
-            type(e).__name__,
-            e,
-        )
+shards_json = client(API_URL, SHARDS)
+health_json = client(API_URL, HEALTH)
 
-def relocating_index(shards_json):
+
+def get_relocating_shards():
+    relocating_shards = client(API_URL, SHARDS)
+    print(shards_json)
+    return relocating_shards['relocating_shards']
+
+
+def relocating_index(shards_json=shards_json):
     shard_index = []
     for shard in shards_json:
         # print(shard['index'])
@@ -83,15 +60,60 @@ def relocating_index(shards_json):
             pass
 
 
+def get_health(key_name):
+    health_value = client(API_URL, HEALTH)
+    # print(health_json)
+    # print(health_value[key_name])
+    return health_value[key_name]
+
+
+def get_stats():
+    stats = client(API_URL, STATS)
+    return stats['indices']['docs']['count']
+
+
+def health_average(start_time):
+    try:
+        loop_start = time.time()
+        count = 10
+        """
+        relocating_shards: 0,
+        initializing_shards: 32,
+        unassigned_shards: 233,
+        """
+        health = [get_health("relocating_shards"), get_health("initializing_shards"), get_health("unassigned_shards")]
+        for stat in
+        while count != 0:
+            time.sleep(2.0 - ((time.time() - loop_start) % 2.0))
+            count -= 1
+            relocating = get_health("relocating_shards")
+            initializing = get_health("initializing_shards")
+            unassigned = get_health("unassigned_shards")
+            doc_count = get_stats()
+            count =
+            print("relocating:", relocating, "initializing:", initializing,
+                  "unassigned:", unassigned, "count:", count)
+            # return health = [relocating_avg, initializing_avg, unassigned_avg, count_avg]
+    except Exception as e:
+        print(
+            "Error on line {}".format(sys.exc_info()[-1].tb_lineno),
+            type(e).__name__,
+            e,
+        )
+
+
 def check():
     start_time = datetime.now()
     # print(start_time)
-    shards_json = client(API_URL, SHARDS)
-    health_json = client(API_URL, HEALTH)
+    # shards_json = client(API_URL, SHARDS)
+    # health_json = client(API_URL, HEALTH)
     # print(prettyfy_json(health))
-    print(relocating_index(shards_json))
-    while health_average(start_time, health_json) != 0:
-        print("still relocating shards ::", relocating_index(shards_json), prettyfy_json(health_json['relocating_shards']), "started at:", start_time)
+    # print(relocating_index(shards_json))
+    health_average(start_time)
+    # while health_average(start_time, health_json) != 0:
+    #     print("still relocating shards ::", relocating_index(shards_json),
+    #           prettyfy_json(health_json['relocating_shards']), "started at:",
+    #           start_time)
 
 
 check()
